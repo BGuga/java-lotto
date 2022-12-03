a9d75d11476dcf6ad570f08f5d39d6ba4fac39c0
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-03 22:56:54 +0900

    feat: 로또는 중복되지 않은 6개의 숫자로 이루어져 있따.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final String LOTTO_SIZE_ERROR_MESSAGE = "[ERROR] 로또는 6자리 숫자로 이루어져 있습니다.";
+    public static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또는 중복되지 않은 숫자들로 생성 됩니다.";
+    private static final int lottoNumberCount = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +15,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        checkLottoNumberCount(numbers);
+        checkDuplication(numbers);
+    }
+
+    private void checkLottoNumberCount(List<Integer> numbers) {
+        if (numbers.size() != lottoNumberCount) {
+            throw new IllegalArgumentException(LOTTO_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void checkDuplication(List<Integer> numbers) {
+        long nonDuplicatedNumberCount = numbers.stream().distinct().count();
+        if (nonDuplicatedNumberCount != numbers.size()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
