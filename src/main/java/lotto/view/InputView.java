0968f90668c3da228996daed64578a57f86b98b2
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-04 00:42:18 +0900

    feat: ,로 나누어지는 로또 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,18 +3,37 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Money;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
     public Money readMoneyByConsole() {
         System.out.println("구입금액을 입력해 주세요.");
-        return new Money(makeMoney(Console.readLine());)
+        return new Money(makeInt(Console.readLine()));
     }
 
-    private int makeMoney(String moneyString) {
+    public List<Integer> readLottoNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return makeStringListToIntList(splitByComma(Console.readLine()));
+    }
+
+    private int makeInt(String moneyString) {
         try {
             return Integer.parseInt(moneyString);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요");
         }
+    }
+
+    private List<Integer> makeStringListToIntList(List<String> data) {
+        return data.stream()
+                .map(ch -> makeInt(ch))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> splitByComma(String lottoNumData) {
+        return Arrays.asList(lottoNumData.split(","));
     }
 }
