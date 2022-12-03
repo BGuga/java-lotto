5711d0a7f4b9282ae25f0a0421292faecbbc1fff
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-03 23:28:51 +0900

    feat: 로또의 결과를 반환하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoResult {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int matchingNumber;
+    private final int prize;
+
+    LottoResult(int matchingNumber, int prize) {
+        this.matchingNumber = matchingNumber;
+        this.prize = prize;
+    }
+
+    public static LottoResult getResult(int matchingCount, boolean bonusNumberMatched) {
+        LottoResult lottoResult = Arrays.stream(values())
+                .filter(result -> result.matchingNumber == matchingCount)
+                .findAny()
+                .get();
+        if (lottoResult == LottoResult.SECOND || lottoResult == LottoResult.THIRD) {
+            return checkSecondOrThird(bonusNumberMatched);
+        }
+        return lottoResult;
+    }
+
+    private static LottoResult checkSecondOrThird(boolean bonusNumberMatched) {
+        if (bonusNumberMatched) {
+            return LottoResult.SECOND;
+        }
+        return LottoResult.THIRD;
+    }
+}
