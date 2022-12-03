9691b93aa5cd78fff9fed83cb3191f22bde3dd2c
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-04 01:29:29 +0900

    feat: 작성된 기능들을 순서에 맞게 컨트롤러 등록

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,40 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void process() {
+        List<Lotto> lottoList = getLottosByConsole();
+        WinningLotto winningLotto = getWinningLottoByConsole();
+        printResult(winningLotto, lottoList);
+    }
+
+    private List<Lotto> getLottosByConsole() {
+        Money money = getMoneyByConsole();
+        List<Lotto> lottos = LottoMachine.getRandomLottos(money);
+        outputView.printBuyingLottos(lottos);
+        return lottos;
+    }
+
+    private Money getMoneyByConsole() {
+        return inputView.readMoneyByConsole();
+    }
+
+    private WinningLotto getWinningLottoByConsole() {
+        Lotto lotto = new Lotto(inputView.readLottoNumbers());
+        LottoNumber lottoNumber = new LottoNumber(inputView.readBonusNumber());
+        return new WinningLotto(lotto, lottoNumber);
+    }
+
+    private void printResult(WinningLotto winningLotto, List<Lotto> lottos) {
+        outputView.printLottoResult(winningLotto.getResults(lottos));
+        outputView.printLottoProfitRate(winningLotto.getProfitRate(lottos));
+    }
 }
